62e77e2b5775bf368741471204cd92ab4bbf0db5
Songphon Priamphimai <66160061@go.buu.ac.th>
2025-08-08 00:32:54 +0700

    chore(docs): setup Swagger UI for API documentation

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNumber,
@@ -7,21 +8,36 @@ import {
   MaxLength,
   Min,
 } from 'class-validator';
-import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
 
 export class CreateRoomDto {
   @IsString()
   @MaxLength(10)
+  @ApiProperty({
+    description: 'The unique room number',
+    example: '101',
+  })
   roomNumber: string;
 
   @IsString()
   @MaxLength(50)
+  @ApiProperty({
+    description: 'The type of the room (e.g., Single, Double, Suite)',
+    example: 'Deluxe',
+  })
   type: string;
 
   @IsNumber()
   @Min(0)
+  @ApiProperty({
+    description: 'The price of the room per night',
+    example: 1500.0,
+  })
   price: number;
 
+  @ApiPropertyOptional({
+    description: 'Indicates if the room is available',
+    example: true,
+  })
   @IsBoolean()
   @IsOptional()
   isAvailable?: boolean; // Optional, defaults to true if not provided
